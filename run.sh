781534f54b2c6b1fb2aa4635724bfc95f0325fc5
LLxick-stack <action@github.com>
2026-03-06 21:23:43 +0000

    update 06.03.2026 21:23

--- run.sh
+++ run.sh
@@ -2,14 +2,16 @@
 cd /opt/vpn2
 ./vpn_checker
 
-# Добавляем заголовок в vpn.txt
 DATE=$(date +'%d.%m.%Y %H:%M')
 HEADER="#profile-title: LLxickVPN
 #support-url: https://t.me/LLxick2
 #profile-update-interval: 1
 #announce: UPD: $DATE | обновление каждые 15 мин | тгк @LLxickVPN
 "
-echo "$HEADER$(cat vpn.txt)" > vpn.txt
+
+for f in vpn.txt vpn_renamed.txt vpn_base64.txt vpn_renamed_base64.txt bobi_vpn.txt bobi_vpn_lite.txt bobi_vpn_base64.txt countries/*.txt; do
+    [ -f "$f" ] && echo "$HEADER$(cat $f)" > "$f"
+done
 
 git add -A
 git commit -m "update $DATE"
